24e514f61278ef2c2f5ce495db10c1a232c04550
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-01-23 23:31:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,16 +1,58 @@
 class Solution {
 public:
-    int maxLength(vector<string>& arr, string str = "", int index = 0) {
-	    
-      //Use set to check if the string contains all unique characters 
-      unordered_set<char>s(str.begin(), str.end());
-        if (s.size() != (str.length())) 
-            return 0;
-     
-        int result = str.length();
-        for (int i = index; i < arr.size(); i++)
-            result = max(result, maxLength(arr, str+arr[i], i+1));
+    int maxLength(std::vector<std::string>& arr) {
+        int maxLen = 0;
+        int n = arr.size();
 
-        return result;
+        for(int i = 0; i < n; i++){
+            std::unordered_set<char> st;
+
+            for(int j = i; j < n; j++){
+                std::string s = arr[j];
+
+                bool isValid = true;
+                for(int k = 0; k < s.length(); k++){
+                    if (st.count(s[k]) > 0) {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid) {
+                    for(int k = 0; k < s.length(); k++){
+                        st.insert(s[k]);
+                    }
+                    maxLen = max(maxLen, static_cast<int>(st.size()));
+                }
+                else {
+                    break; // Break the inner loop if concatenation is not valid
+                }
+            }
+        }
+
+        return maxLen;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxLength(vector<string>& arr, string curr_str = "", int index = 0) {
+	    
+//       //Use set => if the string contains all unique characters 
+//         unordered_set<char>s(curr_str.begin(), curr_str.end());
+//         if (s.size() != (curr_str.length())) 
+//         {
+//              return 0;
+//         }
+     
+//         int result = curr_str.length();
+//         //recursion call
+//         for (int i = index; i < arr.size(); i++){
+//             result = max(result, maxLength(arr, curr_str+arr[i], i+1));
+//         }
+
+//         return result;
+
+//     }
+// };
