cd1e09bf96a02e07529c4532d1effed25e72c556
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-02-16 01:09:17 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.21%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int row = matrix.size();
-        int col = matrix[0].size();
-        int top = 0, left = 0, right = col - 1, bottom = row - 1;
+        vector<int> result;
+        int top = 0;
+        int bottom = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+        int dir = 0;
 
-        while (top <= bottom && left <= right ) {
-            // from top to right
-            for (int i = left; i <= right; i++) {
-                ans.push_back(matrix[top][i]);
+        while (top <= bottom && left <= right) {
+            switch (dir) {
+                case 0: // Right
+                    for (int i = left; i <= right; i++) {
+                        result.push_back(matrix[top][i]);
+                    }
+                    top++;
+                    break;
+                case 1: // Down
+                    for (int i = top; i <= bottom; i++) {
+                        result.push_back(matrix[i][right]);
+                    }
+                    right--;
+                    break;
+                case 2: // Left
+                    for (int i = right; i >= left; i--) {
+                        result.push_back(matrix[bottom][i]);
+                    }
+                    bottom--;
+                    break;
+                case 3: // Top
+                    for (int i = bottom; i >= top; i--) {
+                        result.push_back(matrix[i][left]);
+                    }
+                    left++;
+                    break;
             }
-            top++;
-
-            // Traverse from right to bottom
-            for (int i = top; i <= bottom; i++) {
-                ans.push_back(matrix[i][right]);
-            }
-            right--;
-
-            // Traverse from bottom to left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    ans.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-
-            // Traverse from left to top
-             if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.push_back(matrix[i][left]);
-                }
-                left++;
-            }
+            dir = (dir + 1) % 4;
         }
-        return ans;
+
+        return result;
     }
 };
