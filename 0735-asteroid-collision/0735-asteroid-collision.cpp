5b22c98d538288d9587e09cc5a6cf7051031169c
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-02-28 01:38:11 +0530

    Time: 14 ms (28.27%), Space: 21.3 MB (5.73%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include <stack>
+
+class Solution {
+public:
+    std::vector<int> asteroidCollision(std::vector<int>& asteroids) {
+        int n = asteroids.size();
+        std::stack<int> stack;
+        stack.push(asteroids[n - 1]);
+
+        int i = n - 2;
+        while (i >= 0) {
+            int a = asteroids[i];
+            if (stack.empty()) {
+                stack.push(a);
+                i--;
+                continue;
+            }
+            int b = stack.top();
+            stack.pop();
+
+            if (isSameDirection(a, b)) {
+                stack.push(b);
+                stack.push(a);
+            } else {
+                if (a < 0 && b > 0) {
+                    stack.push(b);
+                    stack.push(a);
+                } else {
+                    if (std::abs(a) > std::abs(b))
+                        stack.push(a);
+                    else if (std::abs(a) < std::abs(b))
+                        stack.push(b);
+                }
+            }
+
+            if (stack.size() > 1) {
+                int a = stack.top();
+                stack.pop();
+                int b = stack.top();
+                stack.pop();
+                stack.push(b);
+                stack.push(a);
+
+                if (!isSameDirection(a, b)) {
+                    if (a < 0 && b > 0) {
+                        // do nothing
+                    } else {
+                        stack.pop();
+                        continue;
+                    }
+                }
+            }
+            i--;
+        }
+
+        std::vector<int> ans;
+        while (!stack.empty()) {
+            ans.push_back(stack.top());
+            stack.pop();
+        }
+
+        return ans;
+    }
+
+    bool isSameDirection(int a, int b) {
+        return (a > 0 && b > 0) || (a < 0 && b < 0);
+    }
+};
