5962b5daa5a6c1384ac3e2826fbb872131402d80
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-02 00:22:02 +0530

    Time: 66 ms (44.17%), Space: 41.9 MB (83.73%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size(); long long ans = 0;
-        stack<int> st; int mod = 1e9+7;
-        for(int i = 0;i<=n;i++){
-            while(st.size() and (i==n or arr[st.top()]>arr[i])){
-                int j = st.top();
-                st.pop();
-                int k = st.empty()?-1:st.top();
-                ans+=(long long) arr[j]*(i-j)*(j-k);
+    int sumSubarrayMins(vector<int>& arr) {
+        long long res =0;
+        stack<int> stack;
+        long long M = 1e9 + 7;
+        stack.push(-1);
+
+        for(int i2=0;i2<arr.size()+1;i2++){
+            int currVal = (i2 < arr.size())?arr[i2]:0;
+
+            while(stack.top()!=-1 && currVal < arr[stack.top()]){
+                int index = stack.top();
+                stack.pop();
+                int i1=stack.top();
+                int left = index - i1;
+                int right = i2 - index;
+                long long add = (long long)(left * right * (long long)arr[index]) % M;
+                res += add;
+                res %= M;
             }
-            st.push(i);
+            stack.push(i2);
         }
-        return (int)(ans%mod);
+        return res;
+
     }
 };
