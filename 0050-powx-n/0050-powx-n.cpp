b640305e81b93728e63236051373827a88b305f1
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-06 23:13:11 +0530

    Time: 0 ms (100%), Space: 8 MB (36.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,7 +3,8 @@ public:
     double myPow(double x, int n) {
         if (n == 0)
             return 1;
-        
+        if (n == 1)
+            return x;
         if (n < 0) {
             n = abs(n);
             x = 1 / x;
