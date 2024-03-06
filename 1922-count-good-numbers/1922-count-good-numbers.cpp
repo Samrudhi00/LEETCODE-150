07ca461f082b45b0f75baef4ae4b63d4442ac3b9
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-06 23:10:47 +0530

    Time: 0 ms (100%), Space: 7.3 MB (6.69%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -3,6 +3,7 @@ class Solution {
 public:
     long long power(long long x, long long y) {
         if (y == 0) return 1;
+        if(y==1)return x;
         long long ans = power(x, y / 2) % MOD;
         ans = (ans * ans) % MOD;
         if (y % 2) {
@@ -10,7 +11,6 @@ public:
         }
         return ans;
     }
-
     int countGoodNumbers(long long n) {
         long long odd = n / 2;
         long long even = n / 2 + n % 2;  
