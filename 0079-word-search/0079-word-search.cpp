f2155bf95fe65516ca3d8d6b1cfc62c702e39eae
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-19 00:08:06 +0530

    Time: 1007 ms (30.68%), Space: 11.8 MB (19.16%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -8,14 +8,14 @@ public:
             return false;
 
         if (word[index] == board[i][j]) {
-            char ch = board[i][j]; // Store the original character
-            board[i][j] = '-'; // Mark the cell as visited
+            char ch = board[i][j]; 
+            board[i][j] = '-'; 
             bool found = helper(i, j - 1, m, n, board, index + 1, word) ||
                          helper(i, j + 1, m, n, board, index + 1, word) ||
                          helper(i - 1, j, m, n, board, index + 1, word) ||
                          helper(i + 1, j, m, n, board, index + 1, word);
             if (!found)
-                board[i][j] = ch; // Revert the cell back to its original character
+                board[i][j] = ch; 
             return found;
         }
         return false;
@@ -24,13 +24,14 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-        if(m * n < word.size())return false;
+        //if(m * n < word.size())return false;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (word[0] == board[i][j]) {
-                    if (helper(i, j, m, n, board, 0, word))
-                        return true;
-                }
+               
+                // if (word[0] == board[i][j]) {
+                     if (helper(i, j, m, n, board, 0, word))
+                       return true;
+                // }
             }
         }
         return false;
