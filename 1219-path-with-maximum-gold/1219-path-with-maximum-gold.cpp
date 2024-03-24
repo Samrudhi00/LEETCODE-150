242b3e39ad3990e30f0f54374dcc1c8f6218e1f5
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-24 23:53:40 +0530

    Time: 241 ms (39.71%), Space: 10.1 MB (28.32%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,47 +1,43 @@
-#include <vector>
-#include <algorithm> // for std::max
-
-using namespace std;
-
 class Solution {
 public:
-    int checkForAllDirections(int i, int j, int maximum_gold, int maximum_path, vector<vector<int>>& grid, int m, int n) {
-        if (i < 0 || j < 0 || i >= m || j >= n) return maximum_path;
+    int explorePaths(int i, int j, vector<vector<int>>& grid) {
         
-        // Save the value of the current cell and mark it as visited
-        int current_value = grid[i][j];
-        grid[i][j] = 0;
-        
-        int localmax=0;
-        // Calculate maximum path by exploring all directions
-        int p=0,q=0;
-        int left = gird[i][j-1];
-        if(i >=m || j >=n)
-         int right = grid[i][j+1];
-        int up = gir
-        int left = checkForAllDirections(i, j - 1, maximum_gold, maximum_path + current_value, grid, m, n);
-        int right = checkForAllDirections(i, j + 1, maximum_gold, maximum_path + current_value, grid, m, n);
-        int up = checkForAllDirections(i - 1, j, maximum_gold, maximum_path + current_value, grid, m, n);
-        int down = checkForAllDirections(i + 1, j, maximum_gold, maximum_path + current_value, grid, m, n);
-        
-        // Restore the original value of the current cell (backtracking)
-        grid[i][j] = current_value;
-        
-        // Return the maximum path
-        return max({left, right, up, down});
-    }
-    
-    int getMaximumGold(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int maximum_gold = 0;
+        // bc
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0)
+            return 0;
+        
+        ////add new gold
+        int currentGold = grid[i][j]; 
+        //mark visited
+        grid[i][j] = 0; 
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // Start exploring from each cell
-                maximum_gold = max(maximum_gold, checkForAllDirections(i, j, 0, 0, grid, m, n));
+        // left , right , up , down
+        int maxGold = currentGold + max({explorePaths(i + 1, j, grid),
+                                         explorePaths(i - 1, j, grid),
+                                         explorePaths(i, j + 1, grid),
+                                         explorePaths(i, j - 1, grid)});
+
+        //restore 0=>6
+        grid[i][j] = currentGold; 
+
+        return maxGold;
+    }
+
+    int getMaximumGold(vector<vector<int>>& grid) {
+        
+        int maxGold = 0;
+
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] != 0) { 
+                    
+                    maxGold = max(maxGold, explorePaths(i, j, grid));
+                }
             }
         }
-        return maximum_gold;
+
+        return maxGold;
     }
 };
