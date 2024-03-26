379b75308055dc9a1c186153b176add3e6c984b5
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-26 22:59:48 +0530

    Time: 7 ms (10.74%), Space: 13.2 MB (27.84%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -6,43 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == nullptr)
-            return ans;
-
+    void levelOrder(TreeNode* root,vector<vector<int>> &result,int count)
+    {
+        if (!root) return;
         queue<TreeNode*> q;
         q.push(root);
-        bool flag = true;
-
-        while(!q.empty()){
-            int n= q.size();
-            vector<int>temp;
-
-            for(int i=0;i<n;i++){
-                temp.push_back(q.front()->val);
-
-                if (q.front()->left != nullptr) {
-                    q.push(q.front()->left);
+        q.push(NULL);
+        vector<int> cur_vec;
+        while(!q.empty()) 
+        {
+            TreeNode* t = q.front();
+            q.pop();
+            if (t==NULL) {
+                if(count%2!=0)
+                {
+                    reverse(cur_vec.begin(),cur_vec.end());
                 }
-                if (q.front()->right != nullptr) {
-                    q.push(q.front()->right);
+                result.push_back(cur_vec);
+                cur_vec.resize(0);
+                count++;
+                if (q.size() > 0) {
+                    q.push(NULL);
                 }
-
+            } else {
+                cur_vec.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
             }
-            if(!flag){
-                reverse(temp.begin(),temp.end());
-            }
-            //push into final list
-            ans.push_back(temp);
         }
-        return ans;
-
+    }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>  result;
+        int count = 0;
+        levelOrder(root, result,count);
+        return result;
     }
 };
