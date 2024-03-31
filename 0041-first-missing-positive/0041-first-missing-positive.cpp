cdac9b7dc6ef511775d6aa23599e253b3daaf7ab
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-03-31 23:17:44 +0530

    Time: 77 ms (17.22%), Space: 60.6 MB (12.02%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -5,15 +5,33 @@ using namespace std;
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> ispresent(nums.begin(), nums.end());
-        
-        int smallestPositive = 1;
-        while (ispresent.find(smallestPositive) != ispresent.end()) {
-            cout<<smallestPositive;
-            smallestPositive++;
-            
+
+        int n = nums.size();
+        unordered_set<int> seen;
+        // Mark presence of positive integers in the array
+        for (int num : nums) {
+            if (num > 0)
+                seen.insert(num);
         }
+        // Find the smallest missing positive integer
+        for (int i = 1; i <= n; ++i) {
+            if (seen.find(i) == seen.end()) {
+                return i;
+            }
+        }
+
+        // If all positive integers from 1 to n are present, return n+1
+        return n + 1;
+
+        // unordered_set<int> ispresent(nums.begin(), nums.end());
         
-        return smallestPositive;
+        // int smallestPositive = 1;
+        // while (ispresent.find(smallestPositive) != ispresent.end()) {
+        //     cout<<smallestPositive;
+        //     smallestPositive++;
+            
+        // }
+        
+        // return smallestPositive;
     }
 };
