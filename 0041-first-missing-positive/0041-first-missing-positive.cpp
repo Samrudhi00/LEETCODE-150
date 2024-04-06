2b5dfbb0b07a452414217bcf54cbf06a3f98f8c6
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-04-07 02:01:38 +0530

    Update 0041-first-missing-positive.cpp

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -5,24 +5,37 @@ using namespace std;
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int x=nums[i];
+            while(x>=1 && x <= n && x!=i+1 && nums[x-1]!=x){
+                swap(nums[x-1],nums[i]);
+                x=nums[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] == i+1)continue;
+            return i+1;
+        }
+        return n+1;
 
        
 
 
         /////////////////////////////////////////////////////////////
-        int n = nums.size();
-        unordered_set<int> seen;
-        // Mark presence of positive integers in the array
-        for (int num : nums) {
-            if (num > 0)
-                seen.insert(num);
-        }
-        // Find the smallest missing positive integer
-        for (int i = 1; i <= n; ++i) {
-            if (seen.find(i) == seen.end()) {
-                return i;
-            }
-        }
+        // int n = nums.size();
+        // unordered_set<int> seen;
+        // // Mark presence of positive integers in the array
+        // for (int num : nums) {
+        //     if (num > 0)
+        //         seen.insert(num);
+        // }
+        // // Find the smallest missing positive integer
+        // for (int i = 1; i <= n; ++i) {
+        //     if (seen.find(i) == seen.end()) {
+        //         return i;
+        //     }
+        // }
 
         // If all positive integers from 1 to n are present, return n+1
         return n + 1;
