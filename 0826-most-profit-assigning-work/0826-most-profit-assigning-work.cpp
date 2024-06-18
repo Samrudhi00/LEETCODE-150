c2aff613e0efe8e029ea7764f9a1e45f05163dd9
Samrudhi Shete <89694069+Samrudhi00@users.noreply.github.com>
2024-06-18 23:20:13 +0530

    Time: 59 ms (40.98%), Space: 39.7 MB (85.44%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int n= profit.size();
-        vector<int2>arr(n);
-        for(int i=0;i<n;i++){
-            arr[i]= {diiiculty[i],profit[i]};
+        int n = profit.size();
+        vector<pair<int, int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            arr[i] = {difficulty[i], profit[i]};
         }
-        sort(arr.begin(),arr.end());
-        sort(worker.begin(),worker.end());
+        sort(arr.begin(), arr.end());
+        sort(worker.begin(), worker.end());
 
-        int mx_profit = 0,index=0,best_Profit=0;
-        
+        int max_profit = 0, index = 0, best_profit = 0;
+
+        for (int i = 0; i < worker.size(); i++) {
+            while (index < n && worker[i] >= arr[index].first) {
+                best_profit = max(best_profit, arr[index].second);
+                index++;
+            }
+            max_profit += best_profit;
+        }
+        return max_profit;
     }
 };
